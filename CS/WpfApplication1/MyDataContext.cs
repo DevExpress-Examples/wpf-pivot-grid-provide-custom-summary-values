9cf040fbedbf0ce8372e2290049a14ae50012d82
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication1/MyDataContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+
+namespace WpfApplication1
+{
+    public class MyDataContext
+    {
+        public MyDataContext()
+        {
+            Data = GetData();
+        }
+
+        public DataTable Data { get; private set; }
+
+        DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            Random _r = new Random();
+            dt.Columns.Add("Row1", typeof(int));
+            dt.Columns.Add("Row2", typeof(int));
+            dt.Columns.Add("Column", typeof(string));
+            dt.Columns.Add("Data", typeof(int));
+            dt.Rows.Add(1, 0, "Column0", _r.Next(10));
+            dt.Rows.Add(2, 0, "Column0", _r.Next(10));
+            dt.Rows.Add(3, 0, "Column0", _r.Next(10));
+            dt.Rows.Add(4, 0, "Column0", _r.Next(10));
+            dt.Rows.Add(1, 1, "Column0", _r.Next(10));
+            dt.Rows.Add(2, 1, "Column0", _r.Next(10));
+            dt.Rows.Add(3, 1, "Column0", _r.Next(10));
+            dt.Rows.Add(4, 1, "Column0", _r.Next(10));
+            dt.Rows.Add(1, 0, "Column1", _r.Next(10));
+            dt.Rows.Add(2, 0, "Column1", _r.Next(10));
+            dt.Rows.Add(3, 0, "Column1", _r.Next(10));
+            dt.Rows.Add(4, 0, "Column1", _r.Next(10));
+            dt.Rows.Add(1, 1, "Column1", _r.Next(10));
+            dt.Rows.Add(2, 1, "Column1", _r.Next(10));
+            dt.Rows.Add(3, 1, "Column1", _r.Next(10));
+            dt.Rows.Add(4, 1, "Column1", _r.Next(10));
+            return dt;
+        }
+
+    }
+}
---
+++ VB/WpfApplication1/MyDataContext.vb
@@ -0,0 +1,47 @@
+﻿Imports System
+Imports System.Data
+
+Namespace WpfApplication1
+    Public Class MyDataContext
+        Public Sub New()
+            Data = GetData()
+        End Sub
+
+        Private privateData As DataTable
+        Public Property Data() As DataTable
+            Get
+                Return privateData
+            End Get
+            Private Set(ByVal value As DataTable)
+                privateData = value
+            End Set
+        End Property
+
+        Private Function GetData() As DataTable
+            Dim dt As New DataTable()
+            Dim _r As New Random()
+            dt.Columns.Add("Row1", GetType(Integer))
+            dt.Columns.Add("Row2", GetType(Integer))
+            dt.Columns.Add("Column", GetType(String))
+            dt.Columns.Add("Data", GetType(Integer))
+            dt.Rows.Add(1, 0, "Column0", _r.Next(10))
+            dt.Rows.Add(2, 0, "Column0", _r.Next(10))
+            dt.Rows.Add(3, 0, "Column0", _r.Next(10))
+            dt.Rows.Add(4, 0, "Column0", _r.Next(10))
+            dt.Rows.Add(1, 1, "Column0", _r.Next(10))
+            dt.Rows.Add(2, 1, "Column0", _r.Next(10))
+            dt.Rows.Add(3, 1, "Column0", _r.Next(10))
+            dt.Rows.Add(4, 1, "Column0", _r.Next(10))
+            dt.Rows.Add(1, 0, "Column1", _r.Next(10))
+            dt.Rows.Add(2, 0, "Column1", _r.Next(10))
+            dt.Rows.Add(3, 0, "Column1", _r.Next(10))
+            dt.Rows.Add(4, 0, "Column1", _r.Next(10))
+            dt.Rows.Add(1, 1, "Column1", _r.Next(10))
+            dt.Rows.Add(2, 1, "Column1", _r.Next(10))
+            dt.Rows.Add(3, 1, "Column1", _r.Next(10))
+            dt.Rows.Add(4, 1, "Column1", _r.Next(10))
+            Return dt
+        End Function
+
+    End Class
+End Namespace
